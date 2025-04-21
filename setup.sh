a79d12fdb1372d52608778e4829106ad979f9f92
Ayush Dumasia <ayushdumasia00@gmail.com>
2025-04-21 18:02:16 +0530

    changes: hypr,setup.sh,

--- setup.sh
+++ setup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 
+cat << EOF | lolcat
+  _________       __   ____ ___         
+ /   _____/ _____/  |_|    |   \______  
+ \_____  \_/ __ \   __\    |   /\____ \ 
+ /        \  ___/|  | |    |  / |  |_> >
+/_______  /\___  >__| |______/  |   __/ 
+        \/     \/               |__|    
+
+EOF
+
 echo "Copying config files..."
 cp -r cava dunst fastfetch gtk-3.0 gtk-4.0 hypr hypridle rofi starship tmux waybar yazi ~/.config/
 
 cp -r bin ~/.local/bin/
+
+cd ~/.local/bin/
+chmod +x *
+
 
 echo "Copying .zshrc..."
 cp zsh/.zshrc ~/.zshrc
@@ -19,4 +33,3 @@ echo "Making scripts executable..."
 chmod +x *
 
 echo "Setup complete!"
-
