0e57120f6c980b7e6cb4c7b4c9b1287409c3ef9f
Ayush Dumasia <ayushdumasia00@gmail.com>
2025-05-11 09:23:29 +0530

    Special Workspace in hyprland

--- rofi/powermenu/powermenu.sh
+++ rofi/powermenu/powermenu.sh
@@ -62,22 +62,14 @@ run_cmd() {
 		elif [[ $1 == '--reboot' ]]; then
 			systemctl reboot
 		elif [[ $1 == '--hibernate' ]]; then
-			systemctl hibernate
+			systemctl suspend
 		elif [[ $1 == '--suspend' ]]; then
 			mpc -q pause
 			amixer set Master mute
 			systemctl suspend
 		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			fi
-		fi
+      hyprctl dispatch exit
+  	fi
 	else
 		exit 0
 	fi
