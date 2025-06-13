cd7248fdc7e5394739cbfacd1445a50d5666e118
tiff <tiff@tifflabs.org>
2025-06-13 00:44:52 -0400

    Update something

--- bin/search.sh
+++ bin/search.sh
@@ -7,18 +7,18 @@ QUERY="$*"
 ENCODED_QUERY=$(echo "$QUERY" | sed 's/ /+/g')
 
 case "$ENGINE" in
-  yt)
-    xdg-open "https://www.youtube.com/results?search_query=$ENCODED_QUERY"
-    ;;
-  wikipedia)
-    xdg-open "https://en.wikipedia.org/wiki/Special:Search?search=$ENCODED_QUERY"
-    ;;
-  duckduckgo)
-    xdg-open "https://duckduckgo.com/?q=$ENCODED_QUERY"
-    ;;
-  *)
-    QUERY="$ENGINE $QUERY"
-    ENCODED_QUERY=$(echo "$QUERY" | sed 's/ /+/g')
-    xdg-open "https://www.google.com/search?q=$ENCODED_QUERY"
-    ;;
+yt)
+  xdg-open "https://www.youtube.com/results?search_query=$ENCODED_QUERY"
+  ;;
+wikipedia)
+  xdg-open "https://en.wikipedia.org/wiki/Special:Search?search=$ENCODED_QUERY"
+  ;;
+searchxng)
+  xdg-open "https://findit.yerp.lat?q=$ENCODED_QUERY"
+  ;;
+*)
+  QUERY="$ENGINE $QUERY"
+  ENCODED_QUERY=$(echo "$QUERY" | sed 's/ /+/g')
+  xdg-open "https://www.google.com/search?q=$ENCODED_QUERY"
+  ;;
 esac
